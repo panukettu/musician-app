a861d64256d1e2b10ee706f4be9779453406e97a
Panu Kettunen <panu.t.kettunen@gmail.com>
2019-11-27 23:53:41 +0200

    add spooky

--- routes/musician.js
+++ routes/musician.js
@@ -9,12 +9,16 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+router.get('/spooky', (req, res) => {
+  res.status('200').send('its spooky')
+})
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
   musician.getMusicians(req.params.id, (err, returnedMusicians) => {
     if (err) {
-      res.status('400').send({errorMessage: err});
+      res.status('400').send({ errorMessage: err });
     }
     res.status('200').send(returnedMusicians);
   });
@@ -25,7 +29,7 @@ router.get('/:id', (req, res) => {
   const { musician } = req.app.locals;
   musician.getMusician(req.params.id, (err, returnedMusician) => {
     if (err) {
-      res.status('400').send({errorMessage: err});
+      res.status('400').send({ errorMessage: err });
     }
     res.status('200').send(returnedMusician);
   });
@@ -35,19 +39,19 @@ router.get('/:id', (req, res) => {
 router.put('/:id', jsonParser, async (req, res) => {
   try {
     const valid = await schema.isValid(req.body);
-    if(valid) {
+    if (valid) {
       const { musician } = req.app.locals;
       musician.putMusician(req.params.id, req.body, (err, id) => {
         if (err) {
-          res.status('400').send({errorMessage: err});
+          res.status('400').send({ errorMessage: err });
         }
-        res.status('200').send({id: id});
+        res.status('200').send({ id: id });
       });
     } else {
-      res.status('400').send({errorMessage: 'Invalid request body'});
+      res.status('400').send({ errorMessage: 'Invalid request body' });
     }
-  } catch(err) {
-    res.status('400').send({errorMessage: 'Invalid request body'});
+  } catch (err) {
+    res.status('400').send({ errorMessage: 'Invalid request body' });
   }
 });
 
@@ -56,7 +60,7 @@ router.delete('/:id', (req, res) => {
   const { musician } = req.app.locals;
   musician.deleteMusician(req.params.id, (err, deletedMusicianId) => {
     if (err) {
-      res.status('400').send({errorMessage: err});
+      res.status('400').send({ errorMessage: err });
     }
     res.status('200').send(deletedMusicianId);
   });
