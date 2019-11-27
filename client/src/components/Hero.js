4508ee03d4983a21ce9e6e2e0ab809be46bd2dfd
Panu Kettunen <panu.t.kettunen@gmail.com>
2019-11-27 23:45:40 +0200

    modified

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero is-primary">
       <div className="hero-body">
         <div className="container">
-          Musician App
+          Musicians
         </div>
       </div>
     </section>
